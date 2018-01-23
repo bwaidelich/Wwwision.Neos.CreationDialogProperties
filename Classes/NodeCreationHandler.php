c60a7fd10187f6a7f35021558e0a4f920ab034bf
Bastian <b.waidelich@wwwision.de>
2018-01-23 13:07:38 +0100

    TASK: Use ProperyMapper to convert creation dialog properties
    if their type differs from the one defined in the NodeType definition

--- Classes/NodeCreationHandler.php
+++ Classes/NodeCreationHandler.php
@@ -2,7 +2,11 @@
 namespace Wwwision\Neos\CreationDialogProperties;
 
 use Neos\ContentRepository\Domain\Model\NodeInterface;
+use Neos\Flow\Annotations as Flow;
+use Neos\Flow\Property\PropertyMapper;
+use Neos\Flow\Property\TypeConverter\PersistentObjectConverter;
 use Neos\Neos\Ui\NodeCreationHandler\NodeCreationHandlerInterface;
+use Neos\Utility\TypeHandling;
 
 /**
  * A Node Creation Handler that takes the incoming data from the Creation Dialog and sets the corresponding node property
@@ -11,14 +15,29 @@ class NodeCreationHandler implements NodeCreationHandlerInterface
 {
 
     /**
+     * @Flow\Inject
+     * @var PropertyMapper
+     */
+    protected $propertyMapper;
+
+    /**
      * @param NodeInterface $node The newly created node
      * @param array $data incoming data from the creationDialog
      * @return void
      */
     public function handle(NodeInterface $node, array $data)
     {
+        $propertyMappingConfiguration = $this->propertyMapper->buildPropertyMappingConfiguration();
+        $propertyMappingConfiguration->forProperty('*')->allowAllProperties();
+        $propertyMappingConfiguration->setTypeConverterOption(PersistentObjectConverter::class, PersistentObjectConverter::CONFIGURATION_OVERRIDE_TARGET_TYPE_ALLOWED, true);
+
         foreach ($data as $propertyName => $propertyValue) {
+            $propertyType = TypeHandling::normalizeType($node->getNodeType()->getPropertyType($propertyName));
+            if ($propertyType !== 'references' && $propertyType !== 'reference' && $propertyType !== TypeHandling::getTypeForValue($propertyValue)) {
+                $propertyValue = $this->propertyMapper->convert($propertyValue, $propertyType, $propertyMappingConfiguration);
+            }
             $node->setProperty($propertyName, $propertyValue);
+
         }
     }
 }
